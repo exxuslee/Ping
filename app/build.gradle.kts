590dd0d1d30080daa4fd46427de50ed7bd72b6a1
Oleksij Romanenko <exxuslee@gmail.com>
2025-12-09 17:52:14 +0200

    Add ping functionality with OkHttp and implement basic UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,6 +13,7 @@ android {
     defaultConfig {
         applicationId = "org.haos.exxuslee.ping"
         minSdk = 26
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
