d8c7e7f39bfc6e0c9db520570bacffdce54b057c
Oleksij Romanenko <exxuslee@gmail.com>
2025-12-10 11:44:04 +0200

    Update version code and name; add Privacy Policy page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "org.haos.exxuslee.ping"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
